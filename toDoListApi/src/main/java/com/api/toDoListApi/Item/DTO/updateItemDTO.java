470f5401766c80dec47ec5824779a33775799e8f
Pedro Lucas Medina Pereira <plmedina2002@gmail.com>
2024-04-11 15:32:39 -0300

    Update updateItemDTO.java

--- toDoListApi/src/main/java/com/api/toDoListApi/Item/DTO/updateItemDTO.java
+++ toDoListApi/src/main/java/com/api/toDoListApi/Item/DTO/updateItemDTO.java
@@ -1,4 +1,42 @@
 package com.api.toDoListApi.Item.DTO;
 
+import java.util.Date;
+
 public class updateItemDTO {
+    private String titleItem;
+    private String description;
+    private Date startDate;
+    private Date finalDate;
+
+    public String getTitleItem() {
+        return titleItem;
+    }
+
+    public void setTitleItem(String titleItem) {
+        this.titleItem = titleItem;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getFinalDate() {
+        return finalDate;
+    }
+
+    public void setFinalDate(Date finalDate) {
+        this.finalDate = finalDate;
+    }
 }
