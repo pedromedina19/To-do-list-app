fea623447af24a472ffa4f17ec089eae17d2377e
Pedro Lucas Medina Pereira <plmedina2002@gmail.com>
2024-04-11 15:07:23 -0300

    Create Entities

--- toDoListApi/src/main/java/com/api/toDoListApi/Item/Entity/itemEntity.java
+++ toDoListApi/src/main/java/com/api/toDoListApi/Item/Entity/itemEntity.java
@@ -1,4 +1,90 @@
 package com.api.toDoListApi.Item.Entity;
 
+
+import com.api.toDoListApi.List.Entity.listEntity;
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "items")
 public class itemEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "start_date")
+    private Date startDate;
+
+    @Column(name = "final_date")
+    private Date finalDate;
+
+    @Column(name = "itemOrder")
+    private Integer itemOrder;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "list_id")
+    private listEntity list;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getFinalDate() {
+        return finalDate;
+    }
+
+    public void setFinalDate(Date finalDate) {
+        this.finalDate = finalDate;
+    }
+
+    public Integer getitemOrder() {
+        return itemOrder;
+    }
+
+    public void setitemOrder(Integer itemOrder) {
+        this.itemOrder = itemOrder;
+    }
+
+    public listEntity getList() {
+        return list;
+    }
+
+    public void setList(listEntity list) {
+        this.list = list;
+    }
 }
--- toDoListApi/src/main/java/com/api/toDoListApi/List/Entity/listEntity.java
+++ toDoListApi/src/main/java/com/api/toDoListApi/List/Entity/listEntity.java
@@ -1,4 +1,56 @@
 package com.api.toDoListApi.List.Entity;
 
+
+import com.api.toDoListApi.Item.Entity.itemEntity;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "lists")
 public class listEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "listOrder")
+    private Integer listOrder;
+
+    @OneToMany(mappedBy = "list", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<itemEntity> items;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getlistOrder() {
+        return listOrder;
+    }
+
+    public void setlistOrder(Integer listOrder) {
+        this.listOrder = listOrder;
+    }
+
+    public List<itemEntity> getItems() {
+        return items;
+    }
+
+    public void setItems(List<itemEntity> items) {
+        this.items = items;
+    }
 }
