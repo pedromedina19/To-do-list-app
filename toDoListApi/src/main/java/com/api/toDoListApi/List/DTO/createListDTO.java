ee6bb4b53fa4ac1112a6f1f41582f3cd221815f7
Pedro Lucas Medina Pereira <plmedina2002@gmail.com>
2024-04-11 15:23:30 -0300

    create istDTO and import hibernate validation on pom

--- toDoListApi/src/main/java/com/api/toDoListApi/List/DTO/createListDTO.java
+++ toDoListApi/src/main/java/com/api/toDoListApi/List/DTO/createListDTO.java
@@ -1,4 +1,17 @@
 package com.api.toDoListApi.List.DTO;
 
+
+import jakarta.validation.constraints.NotEmpty;
+
 public class createListDTO {
+    @NotEmpty(message = "O título da lista não pode ser vazio.")
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
