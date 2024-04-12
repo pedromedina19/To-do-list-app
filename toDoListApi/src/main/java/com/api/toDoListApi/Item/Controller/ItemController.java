90d1dfad41256497c5a8a4464cad28a0eff98bce
Pedro Lucas Medina Pereira <plmedina2002@gmail.com>
2024-04-12 18:46:08 -0300

    fix back-end bugs

--- toDoListApi/src/main/java/com/api/toDoListApi/Item/Controller/ItemController.java
+++ toDoListApi/src/main/java/com/api/toDoListApi/Item/Controller/ItemController.java
@@ -5,15 +5,12 @@ import com.api.toDoListApi.Item.DTO.UpdateItemDTO;
 import com.api.toDoListApi.Item.DTO.UpdateOrderItemDTO;
 import com.api.toDoListApi.Item.Entity.ItemEntity;
 import com.api.toDoListApi.Item.Service.ItemService;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 
 @RestController
 @RequestMapping("/item")
@@ -24,48 +21,31 @@ public class ItemController {
         this.itemService = itemService;
     }
 
-    @Operation(summary = "Cria um novo item", description = "Cria um novo item na lista", responses = {
-            @ApiResponse(responseCode = "201", description = "Item criado com sucesso", content = @Content(schema = @Schema(implementation = ItemEntity.class))),
-            @ApiResponse(responseCode = "400", description = "Requisição inválida")
-    })
+
     @PostMapping
-    public ResponseEntity<ItemEntity> createItem(@Valid @RequestBody(description = "DTO para criar um novo item", required = true, content = @Content(schema = @Schema(implementation = CreateItemDTO.class))) CreateItemDTO createItemDto) {
+    public ResponseEntity<ItemEntity> createItem(@Valid @RequestBody CreateItemDTO createItemDto) {
         ItemEntity newItem = itemService.createItem(createItemDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(newItem);
     }
-    @Operation(summary = "Atualiza um item existente", description = "Atualiza um item existente na lista", responses = {
-            @ApiResponse(responseCode = "200", description = "Item atualizado com sucesso", content = @Content(schema = @Schema(implementation = ItemEntity.class))),
-            @ApiResponse(responseCode = "400", description = "Requisição inválida"),
-            @ApiResponse(responseCode = "404", description = "Item não encontrado")
-    })
+
     @PatchMapping("/{id}")
-    public ResponseEntity<ItemEntity> updateItem(@PathVariable Long id, @Valid @RequestBody(description = "DTO para atualizar um item", required = true, content = @Content(schema = @Schema(implementation = UpdateItemDTO.class))) UpdateItemDTO updateItemDto) {
+    public ResponseEntity<ItemEntity> updateItem(@PathVariable Long id, @Valid @RequestBody UpdateItemDTO updateItemDto) {
         ItemEntity updatedItem = itemService.updateItem(id, updateItemDto);
         return ResponseEntity.ok(updatedItem);
     }
-    @Operation(summary = "Atualiza a ordem de um item existente", description = "Atualiza a ordem de um item existente na lista", responses = {
-            @ApiResponse(responseCode = "200", description = "Ordem do item atualizada com sucesso", content = @Content(schema = @Schema(implementation = ItemEntity.class))),
-            @ApiResponse(responseCode = "400", description = "Requisição inválida"),
-            @ApiResponse(responseCode = "404", description = "Item não encontrado")
-    })
+
     @PatchMapping("/order/{id}")
-    public ResponseEntity<ItemEntity> updateItemOrder(@PathVariable Long id, @RequestBody(description = "DTO para atualizar a ordem de um item", required = true, content = @Content(schema = @Schema(implementation = UpdateOrderItemDTO.class))) UpdateOrderItemDTO dto) {
+    public ResponseEntity<ItemEntity> updateItemOrder(@PathVariable Long id, @RequestBody UpdateOrderItemDTO dto) {
         ItemEntity updatedItem = itemService.updateItemOrder(id, dto);
         return ResponseEntity.ok(updatedItem);
     }
-    @Operation(summary = "Obtém um item", description = "Obtém um item da lista pelo ID", responses = {
-            @ApiResponse(responseCode = "200", description = "Item obtido com sucesso", content = @Content(schema = @Schema(implementation = ItemEntity.class))),
-            @ApiResponse(responseCode = "404", description = "Item não encontrado")
-    })
+
     @GetMapping("/{id}")
     public ResponseEntity<ItemEntity> getOneItem(@PathVariable Long id) {
         ItemEntity item = itemService.getOneItem(id);
         return ResponseEntity.ok(item);
     }
-    @Operation(summary = "Deleta um item", description = "Deleta um item da lista pelo ID", responses = {
-            @ApiResponse(responseCode = "204", description = "Item deletado com sucesso"),
-            @ApiResponse(responseCode = "404", description = "Item não encontrado")
-    })
+
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteItem(@PathVariable Long id) {
--- toDoListApi/src/main/java/com/api/toDoListApi/List/Controller/ListController.java
+++ toDoListApi/src/main/java/com/api/toDoListApi/List/Controller/ListController.java
@@ -5,15 +5,11 @@ import com.api.toDoListApi.List.DTO.UpdateListDTO;
 import com.api.toDoListApi.List.DTO.UpdateOrderListDTO;
 import com.api.toDoListApi.List.Entity.ListEntity;
 import com.api.toDoListApi.List.Service.ListService;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 import java.util.List;
 
 
@@ -25,56 +21,37 @@ public class ListController {
     public ListController(ListService listService) {
         this.listService = listService;
     }
-    @Operation(summary = "Cria uma nova lista", description = "Cria uma nova lista", responses = {
-            @ApiResponse(responseCode = "201", description = "Lista criada com sucesso", content = @Content(schema = @Schema(implementation = ListEntity.class))),
-            @ApiResponse(responseCode = "400", description = "Requisição inválida")
-    })
+
     @PostMapping
-    public ResponseEntity<ListEntity> createList(@Valid @RequestBody(description = "DTO para criar uma nova lista", required = true, content = @Content(schema = @Schema(implementation = CreateListDTO.class))) CreateListDTO createListDto) {
+    public ResponseEntity<ListEntity> createList(@Valid @RequestBody CreateListDTO createListDto) {
         ListEntity newList = listService.createList(createListDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(newList);
     }
-    @Operation(summary = "Atualiza uma lista existente", description = "Atualiza uma lista existente", responses = {
-            @ApiResponse(responseCode = "200", description = "Lista atualizada com sucesso", content = @Content(schema = @Schema(implementation = ListEntity.class))),
-            @ApiResponse(responseCode = "400", description = "Requisição inválida"),
-            @ApiResponse(responseCode = "404", description = "Lista não encontrada")
-    })
+
     @PatchMapping("/{id}")
-    public ResponseEntity<ListEntity> updateList(@PathVariable Long id, @Valid @RequestBody(description = "DTO para atualizar uma lista", required = true, content = @Content(schema = @Schema(implementation = UpdateListDTO.class))) UpdateListDTO updateListDto) {
+    public ResponseEntity<ListEntity> updateList(@PathVariable Long id, @Valid @RequestBody UpdateListDTO updateListDto) {
         ListEntity updatedList = listService.updateList(id, updateListDto);
         return ResponseEntity.ok(updatedList);
     }
-    @Operation(summary = "Atualiza a ordem de uma lista existente", description = "Atualiza a ordem de uma lista existente", responses = {
-            @ApiResponse(responseCode = "200", description = "Ordem da lista atualizada com sucesso", content = @Content(schema = @Schema(implementation = ListEntity.class))),
-            @ApiResponse(responseCode = "400", description = "Requisição inválida"),
-            @ApiResponse(responseCode = "404", description = "Lista não encontrada")
-    })
+
     @PatchMapping("/order/{id}")
-    public ResponseEntity<ListEntity> updateListOrder(@PathVariable Long id, @RequestBody(description = "DTO para atualizar a ordem de uma lista", required = true, content = @Content(schema = @Schema(implementation = UpdateOrderListDTO.class))) UpdateOrderListDTO dto) {
+    public ResponseEntity<ListEntity> updateListOrder(@PathVariable Long id, @RequestBody UpdateOrderListDTO dto) {
         ListEntity updatedList = listService.updateListOrder(id, dto);
         return ResponseEntity.ok(updatedList);
     }
-    @Operation(summary = "Obtém todas as listas", description = "Obtém todas as listas", responses = {
-            @ApiResponse(responseCode = "200", description = "Listas obtidas com sucesso", content = @Content(schema = @Schema(implementation = ListEntity.class)))
-    })
+
     @GetMapping
     public ResponseEntity<List<ListEntity>> getAllLists() {
         List<ListEntity> lists = listService.getAllLists();
         return ResponseEntity.ok(lists);
     }
-    @Operation(summary = "Obtém uma lista", description = "Obtém uma lista pelo ID", responses = {
-            @ApiResponse(responseCode = "200", description = "Lista obtida com sucesso", content = @Content(schema = @Schema(implementation = ListEntity.class))),
-            @ApiResponse(responseCode = "404", description = "Lista não encontrada")
-    })
+
     @GetMapping("/{id}")
     public ResponseEntity<ListEntity> getOneList(@PathVariable Long id) {
         ListEntity list = listService.getOneList(id);
         return ResponseEntity.ok(list);
     }
-    @Operation(summary = "Deleta uma lista", description = "Deleta uma lista pelo ID", responses = {
-            @ApiResponse(responseCode = "204", description = "Lista deletada com sucesso"),
-            @ApiResponse(responseCode = "404", description = "Lista não encontrada")
-    })
+
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteList(@PathVariable Long id) {
         listService.deleteList(id);
